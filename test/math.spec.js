8a6674a3a3215537469dccc092117c639c905762
Karan710 <33235234+Karan710@users.noreply.github.com>
2024-12-09 12:30:16 +0530

    Update math.spec.js

--- test/math.spec.js
+++ test/math.spec.js
@@ -10,7 +10,7 @@ test.describe('Math module', () => {
   });
 
   test('should correctly subtract two numbers', () => {
-    expect(math.subtract(5, 3)).toBe(1);
+    expect(math.subtract(5, 3)).toBe(2);
   });
   test('[Failed Step Demo]Addition with steps', async () => {
     await test.step('Start by Adding Number 1', async () => {
@@ -20,7 +20,7 @@ test.describe('Math module', () => {
       expect(true).toBe(true);
     });
     await test.step('Resulting Number should be 3', async () => {
-      expect(math.add(1,2)).toBe(2);
+      expect(math.add(1,2)).toBe(3);
     });
     expect(true).toBe(true);
   });
